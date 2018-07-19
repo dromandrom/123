6e5653ead71ba85ec6d4bbb3388518d5b94a56fa
1652984844@qq.com <1652984844@qq.com>
2018-07-19 23:01:01 +0800

    this is first commit

---
+++ app/src/main/java/com/caonimamadel/www/social/MainActivity.java
@@ -0,0 +1,13 @@
+package com.caonimamadel.www.social;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
